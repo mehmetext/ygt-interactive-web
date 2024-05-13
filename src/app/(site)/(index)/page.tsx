99622fef304f4e954f5cf5ba04fc1f59c1fce4a7
Mehmet <mehmetext68@gmail.com>
2024-05-13 14:57:30 +0300

    add calculator link to home page

--- src/app/(site)/(index)/page.tsx
+++ src/app/(site)/(index)/page.tsx
@@ -1,12 +1,23 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center gap-10">
-      <Image src="/images/ygt-logo.png" alt="YGT" width={200} height={200} />
-      <h1 className="max-w-lg text-center font-black text-5xl text-custom-orange">
-        YAZILIM GELİŞTİRME TOPLULUĞU
-      </h1>
+      <div className="max-w-4xl container flex flex-col items-center justify-center gap-10">
+        <Image src="/images/ygt-logo.png" alt="YGT" width={200} height={200} />
+        <h1 className="max-w-lg text-center font-black text-5xl text-custom-orange">
+          YAZILIM GELİŞTİRME TOPLULUĞU
+        </h1>
+        <div className="w-full flex flex-col gap-4">
+          <Link
+            href="/hesap-makinesi"
+            className="flex items-center justify-center bg-custom-orange transition-colors hover:bg-custom-blue rounded p-4 text-white font-bold"
+          >
+            HESAP MAKİNESİ
+          </Link>
+        </div>
+      </div>
     </main>
   );
 }
